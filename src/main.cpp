f53744198e24dfdc4bb847e1dac0a0163d5a5a38
dreadedmalinos66 <critd@protonmail.com>
2026-09-23 19:05:16 +0200

    update main.cpp
    Refactor web server handling and task management with optimisations.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
+
+#ifdef ENABLE_TFT
 #include <TFT_eSPI.h>
+#endif
+
 #include <SPI.h>
 #include <SD.h>
 #include <FS.h>
@@ -10,11 +14,102 @@
 #include "esp_system.h"
 #include "esp_random.h"
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #define SD_CS_PIN 5
-WebServer server(80);
 
-String inputBuffer = "";
+// task manager (vTask from rtos is deprecated??)
+struct TaskInfo {
+    const char* name;
+    TaskHandle_t handle;
+    bool running;
+    uint32_t stackHighWater;
+};
 
+#define MAX_TASKS 10
+TaskInfo taskRegistry[MAX_TASKS];
+int taskCount = 0;
+
+// EXPERIMENTAL
+// register a task when it starts
+void register_task(const char* name, TaskHandle_t handle) {
+    if (taskCount < MAX_TASKS) {
+        uint32_t waterMark = uxTaskGetStackHighWaterMark(handle);
+        taskRegistry[taskCount] = {name, handle, true, waterMark};
+        taskCount++;
+        Serial.printf("registered task: %s\n", name);
+    }
+}
+
+// unregister a task when it stops
+void unregister_task(TaskHandle_t handle) {
+    for (int i = 0; i < taskCount; i++) {
+        if (taskRegistry[i].handle == handle) {
+            taskRegistry[i].running = false;
+            Serial.printf("unregistered task: %s\n", taskRegistry[i].name);
+            break;
+        }
+    }
+}
+// EXPERIMENTAL
+
+WebServer* server = nullptr;
+
+char inputBuffer[128];
+uint8_t bufIndex = 0;
+
+// service regitry
+struct ServiceInfo {
+    const char* name;
+    bool running;
+    TaskHandle_t taskHandle;
+};
+// service chunk
+ServiceInfo services[] {
+{"cli_loop", true, nullptr},
+{"web", false, nullptr},
+{"network", false, nullptr},
+{"service", false, nullptr} //custom service (you can add your own)
+};
+
+#define NUM_SERVICES (sizeof(services) / sizeof(services[0]))
+void custom_service(void* parameter){
+    int counter = 0;
+    while (true){
+        counter++;
+        if (counter % 60 == 0) {
+            Serial.println("done exactly nothing for 60 seconds!");
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void start_custom_service(){
+    xTaskCreatePinnedToCore(
+        custom_service,  //task function
+        "custom_service",  //name
+        2048,    //task size
+        NULL,   //parameter
+        1,        //priority
+        &services[4].taskHandle,   //task handle
+        1   //pin to core 1
+    );
+    services[4].running = true;
+    Serial.println("started custom service");
+}
+
+void stop_custom_service(){
+    if (services[4].taskHandle != nullptr){
+        vTaskDelete(services[4].taskHandle);
+        services[4].taskHandle = nullptr;
+        services[4].running = false;
+        Serial.println("stopped custom service");
+    }
+}
+// end of service chunk
+
+#ifdef ENABLE_TFT
 TFT_eSPI tft = TFT_eSPI();
 
 static void backlightOn() {
@@ -34,6 +129,7 @@ void tft_init(){
     tft.setRotation(1);
     tft.fillScreen(TFT_BLACK);
 }
+#endif
 
 void entropy(){
     uint32_t entropy = esp_random();
@@ -140,7 +236,7 @@ void net_init() {
 
 //html local web site
 void handleFileRead() {
-    String path = server.uri();
+    String path = server->uri();
 
     if (path.endsWith("/")) {
         path += "index.html";
@@ -162,10 +258,10 @@ void handleFileRead() {
             contentType = "image/jpeg";
         }
 
-        server.streamFile(file, contentType);
+        server->streamFile(file, contentType);
         file.close();
     } else {
-        server.send(404, "text/plain", "404: file Not Found");
+        server->send(404, "text/plain", "404: file Not Found");
     }
 }
 
@@ -175,11 +271,15 @@ void web_server_init() {
         return;
     }
 
-    server.onNotFound(handleFileRead);
-    server.begin();
-
-    Serial.print("web server running at http://");
-    Serial.println(WiFi.localIP());
+    if (server == nullptr) {
+        server = new WebServer(80);
+        server->onNotFound(handleFileRead);
+        server->begin();
+        Serial.print("web server running at http://");
+        Serial.println(WiFi.localIP());
+    } else {
+        Serial.println("web server already running");
+    }
 }
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
@@ -227,7 +327,7 @@ void processCommand(String command){
 command.trim();
 
 if (command.equalsIgnoreCase("list_commands")){
-    Serial.println("\nlist_commands\ndisable_uart\necho text\nreset\nregen_entropy\nread_file /file/path\nls\nremount\nping web.site\nshow_ip\nmanual\nstart_web_server\nsysinfo\nrestart_network\nrestart_i2c\nrestart_tft\ndraw_tft\nclear_tft");
+    Serial.println("\nlist_commands\ndisable_uart\necho text\nreset\nregen_entropy\nread_file /file/path\nls\nremount\nping web.site\nshow_ip\nstart_web_server\nsysinfo\nrestart_network\nrestart_i2c\nrestart_tft\ndraw_tft\nclear_tft\nps\nservice_list\nservice_start\nservice_stop\n");
 }
 
 else if (command.equalsIgnoreCase("disable_uart")){
@@ -289,12 +389,19 @@ else if (command.equalsIgnoreCase("show_ip")){
     Serial.println(WiFi.localIP());
 }
 
-else if (command.equalsIgnoreCase("manual")){
-    Serial.println("sorry, not implemented yet\nyou can download manual / documentation in releases\n");
-}
-
 else if (command.equalsIgnoreCase("start_web_server")){
     web_server_init();
+}
+
+else if (command.equalsIgnoreCase("stop_web_server")){
+    if (server != nullptr){
+        server->close();
+        delete server;
+        server = nullptr;
+        Serial.println("stopped web server");
+    } else {
+        Serial.println("web server is not running");
+    }
 }
 
 else if (command.equalsIgnoreCase("sysinfo")){
@@ -323,7 +430,7 @@ else if (command.equalsIgnoreCase("restart_network")){
 else if (command.equalsIgnoreCase("restart_i2c")){
     i2c_init();
 }
-// EXPERIMENTAL
+#ifdef ENABLE_TFT
 else if (command.equalsIgnoreCase("restart_tft")){
     Serial.println("remember that for now only ILI9341 compatible displays are supported!");
     tft_init();
@@ -338,7 +445,33 @@ else if (command.equalsIgnoreCase("draw_tft")){
 else if (command.equalsIgnoreCase("clear_tft")){
     tft.fillScreen(TFT_BLACK);
 }
-// EXPERIMENTAL
+#endif
+
+else if (command.equalsIgnoreCase("service_list")) {
+    for (int i = 0; i < NUM_SERVICES; i++) {
+        Serial.printf(" [%s] %-15s (core: %d)\n",
+                      services[i].running ? "run" : "stop",
+                      services[i].name,
+                      services[i].taskHandle ? xTaskGetAffinity(services[i].taskHandle) : -1);
+    }
+    Serial.println("\n");
+}
+
+else if (command.startsWith("service_start ")) {
+    String svc = command.substring(14);
+    svc.trim();
+    if (svc == "web") web_server_init();
+    else if (svc == "custom_service") start_custom_service();
+    else if (svc == "network") net_init();
+    else Serial.printf("unknown service: %s\n", svc.c_str());
+}
+
+else if (command.startsWith("service_stop ")) {
+    String svc = command.substring(13);
+    svc.trim();
+    if (svc == "custom_service") stop_custom_service();
+    else Serial.printf("unknown service: %s\n", svc.c_str());
+}
 
 
 else if (command.length() > 0) {
@@ -354,36 +487,41 @@ void setup() {
     entropy();
     sd_init();
     i2c_init();
-    net_init();
     // add here what function to launch automatically on boot
+
+    register_task("main", xTaskGetCurrentTaskHandle());
 }
 
 void loop() {
+    if (server != nullptr){
+        server->handleClient();
+    }
     while (Serial.available() > 0) {
         char c = Serial.read();
-
         if (c == '\r') {
             continue;
         }
-
         if (c == '\b' || c == 127) {
-            if (inputBuffer.length() > 0) {
-                inputBuffer.remove(inputBuffer.length() - 1);
+            if (bufIndex > 0) {
+                bufIndex--;
                 Serial.print("\b \b");
             }
         }
-        else if (c == '\n') {
+        else if (c == '\n') { // Enter
             Serial.println();
-            if (inputBuffer.length() > 0) {
-                processCommand(inputBuffer);
-                inputBuffer = "";
+            if (bufIndex > 0) {
+                inputBuffer[bufIndex] = '\0';
+                processCommand(String(inputBuffer));
+                bufIndex = 0;
             }
             Serial.print("# ");
         }
         else if (c >= 32 && c <= 126) {
-            inputBuffer += c;
-            Serial.print(c);
+            if (bufIndex < sizeof(inputBuffer) - 1) {
+                inputBuffer[bufIndex] = c;
+                bufIndex++;
+                Serial.print(c);
+            }
         }
     }
-    server.handleClient();
 }
